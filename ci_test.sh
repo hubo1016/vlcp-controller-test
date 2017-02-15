533f095556a5d86bfd6a929c0f2905ed2a05ff36
gaopl <gaopl@jiedaibao.com>
2017-02-15 19:23:03 +0800

    添加travis ci使用的脚本！

---
+++ ci_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -xe
+
+[ -f "/home/travis/virtualenv/${venv}/bin/activate" ] && source /home/travis/virtualenv/${venv}/bin/activate
+
+python -version
+
+venv=python2.7
+
+if [ "${TRAVIS_PYTHON_VERSION}" == "pypy" ]; then
+	venv=pypy2
+else
+	venv=python${TRAVIS_PYTHON_VERSION}
+fi
+
+bash -xe starttest.sh $venv
