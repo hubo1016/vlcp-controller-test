05318e6c5c4972472811faef8f01110b5e5a3797
hubo <hubo1016@126.com>
2018-10-29 18:51:14 +0800

    patch openvswitch to workaround a packaging bug

--- Dockerfile/build_ovs.sh
+++ Dockerfile/build_ovs.sh
@@ -17,6 +17,9 @@ apt-get install -y build-essential fakeroot
 
 apt-get install -y graphviz autoconf automake bzip2 debhelper dh-autoreconf libssl-dev libtool openssl procps python-all python-qt4 python-twisted-conch python-zopeinterface python-six uuid-runtime
 
+# Apply patch to workaround a packaging bug
+(cd openvswitch-${ovs_version} && patch -p1 < ../ovs_package.patch)
+
 (cd openvswitch-${ovs_version}; DEB_BUILD_OPTIONS='parallel=8 nocheck' fakeroot debian/rules binary)
 
 cp *.deb $cache_dir
